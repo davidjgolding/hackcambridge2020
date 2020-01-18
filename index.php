3dd197f3bfea3a69f475365fb7aa6f4183cdfa40
David Golding <43792102+davidjgolding@users.noreply.github.com>
2020-01-18 16:01:24 +0000

    graph

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css" />
+  <meta charset="UTF-8" />
+</head>
+
+<body>
+  <div id="contentsNav">
+           <!--Nav bar-->
+           <nav>
+               <!--Site logo-->
+               <div id="logo">
+                  Knock Knock
+               </div>
+               <!--Slide shortcuts-->
+               <div id="navLinks">
+                   <a href="#" class="unselected">A</a>
+                   <a href="#" class="unselected">B</a>
+                   <a href="#" class="unselected">C</a>
+               </div>
+           </nav>
+           <!--Contents-->
+           <div id="contents">
+             <img src="graph.php" />
+           </div>
+    </div>
+
+
+
+</body>
+</html>
